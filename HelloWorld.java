b4e8c45826512351aa7eb2116346f09cce2414a9
Nikhil <abc@gmail.com>
2024-12-29 08:20:40 +0530

    first

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+private class personal
+{
+	Boolean b=input("Are you alive");
+	if(b==yes)
+	 {
+	  System.out.println("Send me the EEA questions");
+	 }
+	else
+	 {
+	  System.out.println("Rest In Peace");
+	 }
+}
